f59698b7cf979c3990ec8e0231475fb6f28b7396
vinod borole <vborole@gmail.com>
2025-09-08 19:38:42 +0530

    Fix typo in fetch_crypto_prices function to correctly parse JSON response

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def fetch_crypto_prices():
     url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
     response = requests.get(url)
     if response.status_code == 200:
-        data = respons.json()
+        data = response.json()
         prices = {}
         for coin in data:
             prices[coin['name']] = coin['current_price']
